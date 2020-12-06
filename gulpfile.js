a7f8124124118032c99a87c4d5a3e06b6222c574
designcrumble <y13uc147@lnmiit.ac.in>
2020-12-07 02:28:52 +0530

    Add calendly

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,31 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var plumber = require('gulp-plumber');
 
-gulp.task('sass', function () {  
+
+// Starts a BrowerSync instance
+gulp.task('sass', gulp.series( function (done) {  
     gulp.src('scss/**/*.scss')
         .pipe(plumber())
         .pipe(sass({includePaths: ['scss']}))
         .pipe(gulp.dest('css'));
-});
+        done();
 
-gulp.task('browser-sync', function() {
+}));
+
+// Starts a BrowerSync instance
+gulp.task('browser-sync', gulp.series( function(done){
 	port: 2000,  
     browserSync.init(["css/*.css", "js/*.js", "index.html"], {
         server: {
             baseDir: "./"
         }
     });
-});
+    done();
+}));
 
-gulp.task('default', ['sass', 'browser-sync'], function () {  
-    gulp.watch("scss/**/*.scss", ['sass']);
-});
+// Starts a BrowerSync instance
+gulp.task('default', gulp.series( ['sass', 'browser-sync'], function(done){
+    gulp.watch('scss/**/*.scss', gulp.series('sass'));
+    done();
+}));
+
